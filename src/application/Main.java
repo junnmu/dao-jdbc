4b4305119192fca4aa49df143d59f436cbdb2cf7
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2020-11-18 16:41:00 -0300

    insert implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -23,5 +23,10 @@ public class Main {
         System.out.println("\n=== TEST 3: seller findAll ===");
         list = sellerDao.findAll();
         list.forEach(System.out::println);
+
+        System.out.println("\n=== TEST 4: seller insert ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New Id = " + newSeller.getId());
     }
 }
