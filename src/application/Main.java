b0a94c436f50b1ea96948ea9ffa28f58ad0adab8
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2020-11-18 15:25:30 -0300

    findById implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,12 +9,8 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Department obj = new Department(1, "Books");
-        System.out.println(obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
-
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 5000.0, obj);
+        Seller seller = sellerDao.findById(3);
         System.out.println(seller);
     }
 }
